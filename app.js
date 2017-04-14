228874b7318a3ffe2888beff968a6f4c25c21817
codepadma <padma.govindarajalu@gmail.com>
2017-04-14 13:34:38 -0400

    Skeleton for view management

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const add = require('./adder.js');
-console.log(add(5, 1));
+//console.log(add(5, 1));
+class ViewManager {
+  connectEventHandlers() {
+  	// wire up event handler for form submit
+  	
+  }
+
+  onSubmit(event) {
+  	// prevent page from submitting
+
+  	// get the input numbers as strings
+
+  	// cast the string to integer
+
+  	// add the numbers
+  	
+  	// output
+
+  }
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
