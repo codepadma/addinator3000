173e5922db5b689f778f47b8175496702f70471c
codepadma <padma.govindarajalu@gmail.com>
2017-04-17 17:39:02 -0700

    Added code in the server to parse the urlpath and load the resource. Also added the edge cases

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 const port = 3000;
 
 const server = http.createServer(function(request, response) {
-  response.end('Hello World!');
+  const path = url.parse(request.url).pathname;
+  const filePath = `.${path}`;
+  fs.exists(filePath, (doesExist) => {
+  	if (!doesExist) {
+      response.statusCode = 404;
+      response.end(`Resource does not exist: "${path}"`);
+  	}
+  });
+  fs.readFile(filePath, (err, data) => {
+  	if (err) {
+  	  response.statusCode = 500;
+      response.end(`Server error: "${err}"`);
+  	} else {
+      response.end(data.toString('UTF-8'));
+  	}
+  });
 });
 
 server.listen(port, function() {
