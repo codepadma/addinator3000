41f54ec45cf0e3ba496fe59beafe05af8e43b091
codepadma <padma.govindarajalu@gmail.com>
2017-04-18 08:02:04 -0700

    Renamed client/adder.html to client/index.html and modified server/server.js to handle error scenarios

--- server/server.js
+++ server/server.js
@@ -5,22 +5,28 @@ const port = 3000;
 
 const server = http.createServer(function(request, response) {
   const path = url.parse(request.url).pathname;
-  const filePath = `.${path}`;
-  fs.exists(filePath, (doesExist) => {
-  	if (!doesExist) {
-      response.statusCode = 404;
-      response.end(`Resource does not exist: "${path}"`);
+  let filePath = `./client/${path}`;
+  fs.stat(filePath, (err, stats) => {
+  	if (!err && stats.isDirectory()) {
+  	  filePath += '/index.html';
   	}
-  });
-  fs.readFile(filePath, (err, data) => {
-  	if (err) {
-  	  response.statusCode = 500;
-      response.end(`Server error: "${err}"`);
-  	} else {
-      response.end(data.toString('UTF-8'));
-  	}
-  });
+  	fs.exists(filePath, (doesExist) => {
+  	  if (!doesExist) {
+        response.statusCode = 404;
+        response.end(`Resource does not exist: "${path}"`);
+  	  }
+  	  fs.readFile(filePath, (err, data) => {
+   	    if (err) {
+  	      response.statusCode = 500;
+          response.end(`Server error: "${err}"`);
+  	    } else {
+          response.end(data.toString('UTF-8'));
+  	    }
+      });
+    });
 });
+  });
+  
 
 server.listen(port, function() {
 	console.log('Server listening...');
