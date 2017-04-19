d1566301c1c696b57b3cde9e22b9bebfaceeebaa
codepadma <padma.govindarajalu@gmail.com>
2017-04-19 10:51:33 -0700

    aligned the code

--- server/server.js
+++ server/server.js
@@ -7,28 +7,28 @@ const server = http.createServer(function(request, response) {
   const path = url.parse(request.url).pathname;
   let filePath = `./client/${path}`;
   fs.stat(filePath, (err, stats) => {
-  	if (!err && stats.isDirectory()) {
-  	  filePath += '/index.html';
-  	}
-  	fs.exists(filePath, (doesExist) => {
-  	  if (!doesExist) {
+    if (!err && stats.isDirectory()) {
+      filePath += '/index.html';
+    }
+    fs.exists(filePath, (doesExist) => {
+      if (!doesExist) {
         response.statusCode = 404;
         response.end(`Resource does not exist: "${path}"`);
-  	  } else {
-  	    fs.readFile(filePath, (err, data) => {
-   	      if (err) {
-  	        response.statusCode = 500;
-            response.end(`Server error: "${err}"`);
-  	      } else {
-            response.end(data.toString('UTF-8'));
-  	      }
+      } else {
+        fs.readFile(filePath, (err, data) => {
+   	if (err) {
+  	  response.statusCode = 500;
+          response.end(`Server error: "${err}"`);
+  	} else {
+          response.end(data.toString('UTF-8'));
+  	}
         });
-  	  } 
+      } 
     });
   });
 });
   
 const PORT = process.env.PORT || 3000;
 server.listen(port, function() {
-	console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
